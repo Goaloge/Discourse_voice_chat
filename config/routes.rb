9b609a1ba89f54a7593bf96b2c95b523fe19857c
Goaloge <140845317+Goaloge@users.noreply.github.com>
2025-05-23 18:17:01 +0200

    Delete config directory

--- config/routes.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-DiscourseVoiceMessages::Engine.routes.draw do
-  post "/voice_messages" => "voice_messages#create"
-  get "/voice_messages/:id" => "voice_messages#show"
-end
